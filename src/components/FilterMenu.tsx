a71bb13022eee2376b59b49e759e1e6e00552b86
Osman Gündüz <4242osman424@gmail.com>
2025-07-10 23:00:53 +0300

    .

--- src/components/FilterMenu.tsx
+++ src/components/FilterMenu.tsx
@@ -4,13 +4,13 @@ import type { FilterProps } from "../types/MiniSlider";
 const FilterMenu: React.FC<FilterProps> = ({ filters, filter, setFilter }) => {
   return (
     <div className="flex justify-center">
-      <div className="flex gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {filters.map((filt, idx) => (
-          <div key={`${filt.name}-${idx}`} className={`flex pb-1 items-center gap-1  cursor-pointer hover:text-blue-400 ${filt.name === filter ? "text-white  border-b-1 border-white" : "text-zinc-400"}`} onClick={() => {
+          <div key={`${filt.name}-${idx}`} className={`flex pb-1 items-center gap-1 text-[16px] justify-center  cursor-pointer hover:text-blue-400 ${filt.name === filter ? "text-white  border-b-1 border-white" : "text-zinc-400"}`} onClick={() => {
             setFilter(filt.name);
           }}>
             {filt.icon}
-            <span className="text-sm font-semibold">{filt.name}</span>
+            <span className="font-semibold">{filt.name}</span>
           </div>
         ))}
       </div>
--- src/components/MiniSlider.tsx
+++ src/components/MiniSlider.tsx
@@ -8,13 +8,27 @@ const MiniSlider: React.FC<MiniSliderProps> = ({ data, filters }) => {
   const [filter, setFilter] = useState<string>(filters[0].name);
   const filteredData = data.filter((item) => item.filter.includes(filter));
   const [currentIndex, setCurrentIndex] = useState(0);
-  const visibleCards = 6;
+  const [visibleCards, setVisibleCards] = useState<number>(3);
   const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
 
-  const maxIndex = Math.max(filteredData.length - visibleCards, 0);
+  useEffect(() => {
+    const updateVisibleCards = () => {
+      const width = window.innerWidth;
+      if (width < 1024) {
+        setVisibleCards(3);
+      } else {
+        setVisibleCards(6);
+      }
+    };
+    updateVisibleCards();
+    window.addEventListener("resize", updateVisibleCards);
+    return () => window.removeEventListener("resize", updateVisibleCards);
+  }, []);
 
   const goNext = () => {
-    setCurrentIndex((prev) => (prev >= maxIndex ? 0 : prev + 1));
+    setCurrentIndex((prev) =>
+      prev >= Math.max(filteredData.length - visibleCards, 0) ? 0 : prev + 1
+    );
   };
 
   const handleNext = () => {
@@ -23,7 +37,9 @@ const MiniSlider: React.FC<MiniSliderProps> = ({ data, filters }) => {
   };
 
   const handlePrev = () => {
-    setCurrentIndex((prev) => (prev <= 0 ? maxIndex : prev - 1));
+    setCurrentIndex((prev) =>
+      prev <= 0 ? Math.max(filteredData.length - visibleCards, 0) : prev - 1
+    );
     resetInterval();
   };
 
@@ -53,7 +69,7 @@ const MiniSlider: React.FC<MiniSliderProps> = ({ data, filters }) => {
         <div className="relative overflow-hidden w-full">
           {/* İçerik alanı */}
           <div
-            className="flex gap-4 transition-transform duration-700 ease-in-out"
+            className="flex gap-2 lg:gap-4 transition-transform duration-700 ease-in-out"
             style={{
               transform: `translateX(${translateX}%)`,
             }}
@@ -61,19 +77,21 @@ const MiniSlider: React.FC<MiniSliderProps> = ({ data, filters }) => {
             {filteredData.map((item, index) => (
               <div
                 key={`${item.title}-${index}`}
-                className="relative flex flex-col flex-shrink-0 w-[15.66%] rounded-b-lg group overflow-hidden cursor-pointer"
+                className="relative flex flex-col flex-shrink-0 rounded-b-lg group overflow-hidden cursor-pointer" style={{width:`${ window.innerWidth > 1024 ? (100 / visibleCards) - 1 : (100/visibleCards) - 2}%`}}
               >
                 <img
                   src={item.imageUrl}
                   alt={item.title}
                   className="w-full h-full object-cover rounded-md group-hover:scale-110 transition-transform duration-300"
                 />
-                <div className="flex flex-col text-[12px] items-center pt-2 gap-1 w-full h-20 bottom-0 bg-slate-800 rounded-b-lg z-10">
+                <div className="flex flex-col text-[12px] items-center pt-2 pb-6 gap-1 w-full bottom-0 bg-slate-800 rounded-b-lg z-10">
                   {item.title}
-                  <div className="flex justify-center w-full h-full font-bold">
-                    <span className="text-sm text-white">{item.price.toFixed(2)} TL</span>
+                  <div className="flex flex-col sm:flex-row items-center justify-center w-full h-full font-bold">
+                    <span className="text-sm text-white">
+                      {item.price.toFixed(2)} TL
+                    </span>
                     <span className="text-sm font-normal text-zinc-400 line-through ml-2">
-                      {(item.price * (1 - (item.discount / 100))).toFixed(2)} TL
+                      {(item.price * (1 - item.discount / 100)).toFixed(2)} TL
                     </span>
                   </div>
                 </div>
