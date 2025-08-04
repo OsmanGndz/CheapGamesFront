5b19ed342b2d87d29f8786674baa919ea436a254
Osman Gündüz <4242osman424@gmail.com>
2025-08-05 00:25:02 +0300

    deploying

--- src/pages/Account/AccountMobile.tsx
+++ src/pages/Account/AccountMobile.tsx
@@ -32,7 +32,7 @@ const AccountMobile = () => {
     <div className="flex flex-col gap-8 w-full px-4 sm:px-20 md:px-40 lg:hidden">
       <h1 className="text-3xl font-bold text-center">Hesabım</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 items-center justify-center w-full gap-4">
-        {choices.map((item, idx) => (
+        {choices.map((item) => (
           <Link
             to={item.url}
             className="w-full flex items-center justify-center border-1 h-24 gap-4 p-4"
